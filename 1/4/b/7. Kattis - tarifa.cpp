bf12693eb091947e1917f310c3d3358d26d2f7ac
mahyar <mahyarstate@gmail.com>
2024-07-16 19:51:55 +0330

    [ACCEPTED] One pass; array not needed
    (MY NOTES)
    - Pero will have received (N + 1) * X megabytes income. But Pero uses Y megabytes each month. So by subtracting Y values, we'll get the result!

--- 1/4/b/7. Kattis - tarifa.cpp
+++ 1/4/b/7. Kattis - tarifa.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 
 int main() {
+    int x, n;
+    cin >> x >> n;
 
+    int m = x * (n + 1);
+    for (int i = 0; i < n; ++i) {
+        int y;
+        cin >> y;
+
+        m -= y;
+    }
+
+    cout << m;
 }
