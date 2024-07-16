81f0698edddd57bff5cfa3fa18889267f4302bd5
mahyar <mahyarstate@gmail.com>
2024-07-16 20:00:11 +0330

    [ACCEPTED] if-else; 2 cases; check n >= 3

--- 1/4/c/1. Kattis - moscowdream.cpp
+++ 1/4/c/1. Kattis - moscowdream.cpp
@@ -3,5 +3,12 @@
 using namespace std;
 
 int main() {
+    int a, b, c, n;
+    cin >> a >> b >> c >> n;
 
+    bool flag1 = (a + b + c) >= n;
+    bool flag2 = a > 0 && b > 0 && c > 0;
+    bool flag3 = n >= 3;
+
+    (flag1 && flag2 && flag3) ? cout << "YES" : cout << "NO";
 }
