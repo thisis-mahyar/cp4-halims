793349545161846d2ced8bd0a33ad94354cde752
mahyar <mahyarstate@gmail.com>
2024-07-16 19:49:17 +0330

    [ACCEPTED] Trivial loop

--- 1/4/b/6. Kattis - qaly.cpp
+++ 1/4/b/6. Kattis - qaly.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
 
+    double a, b, result = 0;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> a >> b;
+        result += a * b;
+    }
+
+    cout << result << "\n";
 }
