e65116cf3f3f8cb4b19b96a09312aa6299535bf2
Mahyar <mahyarstate@gmail.com>
2024-07-16 22:53:44 +0330

    [ACCEPTED] if-else if-else; 3 cases

--- 1/4/c/3. Kattis - judgingmoose.cpp
+++ 1/4/c/3. Kattis - judgingmoose.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main() {
+    int l, r;
+    cin >> l >> r;
 
+    if (r == 0 && l == 0)
+        cout << "Not a moose";
+    else {
+        (l == r) ? cout << "Even " : cout << "Odd ";
+        cout << max(l, r) * 2;
+    }
 }
