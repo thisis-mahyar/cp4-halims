67eff8b80fc78e7908a600809911251ec99303a7
Mahyar <mahyarstate@gmail.com>
2024-08-12 11:41:53 +0330

    [ACCEPTED] if-else if-else; 6 cases [MY NOTES] Pretty complex content

--- 1/4/c/5. Kattis - provincesandgold.cpp
+++ 1/4/c/5. Kattis - provincesandgold.cpp
@@ -3,5 +3,25 @@
 using namespace std;
 
 int main() {
+    int g, s, c;
+    cin >> g >> s >> c;
 
+    int val = 3 * g + 2 * s + c;
+
+    if (val >= 8)
+        cout << "Province";
+    else if (val >= 5)
+        cout << "Duchy";
+    else if (val >= 2)
+        cout << "Estate";
+
+    if (val >= 2)
+        cout << " or ";
+
+    if (val >= 6)
+        cout << "Gold";
+    else if (val >= 3)
+        cout << "Silver";
+    else
+        cout << "Copper";
 }
