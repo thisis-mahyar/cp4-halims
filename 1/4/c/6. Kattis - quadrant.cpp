75b64da4005dfb5f0179eeebdfc3870ace57a54b
Mahyar <mahyarstate@gmail.com>
2024-08-12 11:44:23 +0330

    [ACCEPTED] if-else if-else; 4 cases

--- 1/4/c/6. Kattis - quadrant.cpp
+++ 1/4/c/6. Kattis - quadrant.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main() {
+    int x, y;
+    cin >> x >> y;
 
+    if (x > 0 && y > 0)
+        cout << "1\n";
+    else if (x < 0 && y > 0)
+        cout << "2\n";
+    else if (x < 0 && y < 0)
+        cout << "3\n";
+    else if (x > 0 && y < 0)
+        cout << "4\n";
 }
