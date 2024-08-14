79428bd8306ceae41a6f0e541693e9a23c6f56ce
Mahyar <mahyarstate@gmail.com>
2024-08-15 00:41:04 +0330

    [ACCEPTED] 2 cases

--- 1/4/d/1. Kattis - oddities.cpp
+++ 1/4/d/1. Kattis - oddities.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
 
+    while (n--) {
+        int x;
+        cin >> x;
+
+        (x % 2 == 0) ? printf("%d is even\n", x) : printf("%d is odd\n", x);
+    }
 }
