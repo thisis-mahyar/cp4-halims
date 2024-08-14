ae3afa11081165a97d6dbbbd143055d72b296c3c
Mahyar <mahyarstate@gmail.com>
2024-08-14 23:05:54 +0330

    [ACCEPTED] if-else if-else; 3 cases; derive formula
    (MY NOTES)
    - Consider two states:
        - A == t and B == t (in this case, there are infinite answers)
        - A == t and B == t + k (in this case, the graph of A and aB will be parallel and there won't be any answer)
    - The question might seem awkward due to complying with the precision. But here we are!
    - About "cout << fixed":
        Displays the number in a fixed decimal format (e.g., 123.456) instead of potentially using scientific notation (e.g., 1.23456e+02).
    - About "cout << precision(9)":
        Sets the number of digits to display after the decimal point in floating-point and fixed-mode output.

--- 1/4/c/7. Kattis - temperature.cpp
+++ 1/4/c/7. Kattis - temperature.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 
 int main() {
+    double x, y;
+    cin >> x >> y;
 
+    if (y == 1 && x != 0)
+        cout << "IMPOSSIBLE\n";
+    else if (y == 1 && x == 0)
+        cout << "ALL GOOD\n";
+    else {
+        cout << fixed;
+        cout.precision(9);
+        cout << x / (1 - y) << "\n";
+    }
 }
