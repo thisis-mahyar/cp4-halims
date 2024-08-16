1f442ebdae668332b7dd17367935fb913e52245d
mahyar <mahyarstate@gmail.com>
2024-08-16 20:22:40 +0330

    [ACCEPTED] 2 cases
    (MY NOTES)
    - Unfortunately, C++ doesn't provide a built-in function to split strings. Therefore, we need to implement our own function.
    - splitString() Description:
        First, we convert the string into a stream.
        Then, using getline(), we split the stream by the specified delimiter.
        Finally, we store the extracted data in a vector.

--- 1/4/d/6. Kattis - helpaphd.cpp
+++ 1/4/d/6. Kattis - helpaphd.cpp
@@ -2,6 +2,32 @@
 
 using namespace std;
 
-int main() {
+vector<string> splitString(const string &str, char delimiter) {
+    istringstream stream(str);
+    vector<string> tokens;
+    string token;
 
+    while (getline(stream, token, delimiter))
+        tokens.push_back(token);
+
+    return tokens;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    while (n--) {
+        string s;
+        cin >> s;
+
+        if (s == "P=NP")
+            cout << "skipped\n";
+        else {
+            int a = stoi(splitString(s, '+')[0]);
+            int b = stoi(splitString(s, '+')[1]);
+
+            cout << a + b << "\n";
+        }
+    }
 }
