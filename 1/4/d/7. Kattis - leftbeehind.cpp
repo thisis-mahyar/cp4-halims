0fa8bffe2c4f336a2d13cd2c69b2e84f07ca65ac
mahyar <mahyarstate@gmail.com>
2024-08-24 23:22:56 +0330

    [ACCEPTED] 4 cases

--- 1/4/d/7. Kattis - leftbeehind.cpp
+++ 1/4/d/7. Kattis - leftbeehind.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main() {
+    int x, y;
 
+    while (true) {
+        cin >> x >> y;
+        if (x == 0 && y == 0) break;
+
+        if (x + y == 13)
+            cout << "Never speak again.\n";
+        else if (x < y)
+            cout << "Left beehind.\n";
+        else if (x > y)
+            cout << "To the convention.\n";
+        else if (x == y)
+            cout << "Undecided.\n";
+    }
 }
