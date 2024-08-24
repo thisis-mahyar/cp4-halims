0ad04b79747c39a34f4ade79d8c2eb1cded2280c
mahyar <mahyarstate@gmail.com>
2024-08-24 23:52:15 +0330

    [ACCEPTED] one pass; array not needed
    (MY NOTES)
    - There is even a better approach to obtain min_num and max_num, by comparing two adjacent elements. But let's not make this more complicated!

--- 1/4/e/1. Kattis - statistics.cpp
+++ 1/4/e/1. Kattis - statistics.cpp
@@ -3,5 +3,18 @@
 using namespace std;
 
 int main() {
+    int n;
 
+    for (int i = 1; cin >> n; ++i) {
+        int x, min_num = INT32_MAX, max_num = INT32_MIN;
+
+        for (int j = 0; j < n; ++j) {
+            cin >> x;
+
+            min_num = min(min_num, x);
+            max_num = max(max_num, x);
+        }
+
+        cout << "Case " << i << ": " << min_num << " " << max_num << " " << max_num - min_num << "\n";
+    }
 }
