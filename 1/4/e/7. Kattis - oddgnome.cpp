03fd8c11fdf8dd3797a15e8ec4b1190e36b389b6
Mahyar <mahyarstate@gmail.com>
2024-08-25 11:03:57 +0330

    [ACCEPTED] linear pass
    (MY NOTES)
    - Again, no array is needed. Just use the current and previous values.

--- 1/4/e/7. Kattis - oddgnome.cpp
+++ 1/4/e/7. Kattis - oddgnome.cpp
@@ -3,5 +3,23 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
 
+    while (n--) {
+        int g, pre, index;
+        cin >> g >> pre;
+
+        for (int i = 1; i < g; ++i) {
+            int current;
+            cin >> current;
+
+            if (current - pre != 1)
+                index = i;
+
+            pre = current;
+        }
+
+        cout << index << "\n";
+    }
 }
