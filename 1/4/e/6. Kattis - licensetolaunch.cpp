5bd831f8bea7a11eab44ab1f1192aedfa7379aac
Mahyar <mahyarstate@gmail.com>
2024-08-25 10:49:36 +0330

    [ACCEPTED] easy linear pass
    (MY NOTES)
    - One could create an array and traverse it twice to find the answer, but one traversal is enough, and no array is needed.
    - It is better to initialize the primary value of min_num to the first number rather than to INT32_MAX.

--- 1/4/e/6. Kattis - licensetolaunch.cpp
+++ 1/4/e/6. Kattis - licensetolaunch.cpp
@@ -3,5 +3,18 @@
 using namespace std;
 
 int main() {
+    int n, min_num, min_num_i = 0;
+    cin >> n >> min_num;
 
+    for (int i = 1; i < n; ++i) {
+        int x;
+        cin >> x;
+
+        if (x < min_num) {
+            min_num = x;
+            min_num_i = i;
+        }
+    }
+
+    cout << min_num_i;
 }
