5228e15ea04dec19b6afef27ebccdf87c59dc43f
Mahyar <mahyarstate@gmail.com>
2024-11-27 12:57:35 +0330

    [ACCEPTED] create a 'reverse string' function; then if-else check
    (MY NOTES)
    I reversed the numbers in integer form. But if the number is bigger, string form is better

--- 1/4/f/7. Kattis - filip.cpp
+++ 1/4/f/7. Kattis - filip.cpp
@@ -3,5 +3,11 @@
 using namespace std;
 
 int main() {
+    int a, b;
+    cin >> a >> b;
 
+    a = a % 10 * 100 + a / 10 % 10 * 10 + a / 100;
+    b = b % 10 * 100 + b / 10 % 10 * 10 + b / 100;
+
+    a < b ? cout << b : cout << a;
 }
