c964ebe659d90c1079a41ccb35325c3093ae300f
Mahyar <mahyarstate@gmail.com>
2024-11-27 12:40:40 +0330

    [ACCEPTED] linear scan
    (MY NOTES)
    - First of all, you need to obtain the prices directly from the formula. You have no other choice.
    - But then, how can we find maximum difference between a_i, a_j (i < j)? First solution is to check every pair's difference. But that leads to O(n^2) and exceeds to time limit. What is the better way?
    
    - In this example: {133, 81, 38, 42, 141, 54, 91, 149, 39, 37}
    we first take 133 as max_element and calculate the max_diff with every next element. But when we reach to an element bigger than 133 (141), we update the max_element (= 141) and continue calculating the max_diff with 141. Why we don't continue with that 133? Because after, if we would reach to an element a_k which 133 - a_k > max_diff, then certainly 141 - a_k > 133 - a_k > max_diff. So, why continue with 133 when we can have 141?!

--- 1/4/f/5. Kattis - artichoke.cpp
+++ 1/4/f/5. Kattis - artichoke.cpp
@@ -3,5 +3,17 @@
 using namespace std;
 
 int main() {
+    int p, a, b, c, d, n;
+    cin >> p >> a >> b >> c >> d >> n;
 
+    double max_diff = 0;
+    double max_element = p * (sin(a * 1 + b) + cos(c * 1 + d) + 2);
+
+    for (int k = 2; k <= n; k++) {
+        double next_element = p * (sin(a * k + b) + cos(c * k + d) + 2);
+        if (max_element - next_element > max_diff) max_diff = max_element - next_element;
+        if (next_element > max_element) max_element = next_element;
+    }
+
+    printf("%.7f\n", max_diff);
 }
