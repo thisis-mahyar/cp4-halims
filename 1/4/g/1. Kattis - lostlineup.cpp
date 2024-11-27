79006d3b9db743ca24a2be491869049535a6d126
Mahyar <mahyarstate@gmail.com>
2024-11-27 13:00:52 +0330

    [ACCEPTED] simple 1D array manipulation
    (MY NOTES)
    - Good one!

--- 1/4/g/1. Kattis - lostlineup.cpp
+++ 1/4/g/1. Kattis - lostlineup.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
 
+    vector<int> a(n), b(n);
+
+    for (int i = 0; i < n - 1; ++i)
+        cin >> a[i];
+
+    b[0] = 1;
+
+    for (int i = 0; i < n - 1; ++i)
+        b[a[i] + 1] = i + 2;
+
+    for (int i = 0; i < n; ++i)
+        cout << b[i] << " ";
 }
