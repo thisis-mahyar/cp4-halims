fa6f77b3ca85cb96d51f656a63ce3b868d0b346c
Mahyar <mahyarstate@gmail.com>
2024-11-27 12:55:00 +0330

    [ACCEPTED] direct simulation
    (MY NOTES)
    Examples:
    x.length() > 9: 1e6 digits -> 7 digits -> 1 digit -> 1 digit
    x.length() > 1: 24634624 (8 digits) -> 1 digit -> 1 digit
    x != 1: 9 (1 digit) -> 1 digit
    x == 1: 1 digit

--- 1/4/f/6. Kattis - digits.cpp
+++ 1/4/f/6. Kattis - digits.cpp
@@ -3,5 +3,20 @@
 using namespace std;
 
 int main() {
+    string x;
 
+    while (true) {
+        cin >> x;
+        if (x == "END")
+            break;
+
+        if (x.length() > 9)
+            cout << "4\n";
+        else if (x.length() > 1)
+            cout << "3\n";
+        else if (x != "1")
+            cout << "2\n";
+        else
+            cout << "1\n";
+    }
 }
